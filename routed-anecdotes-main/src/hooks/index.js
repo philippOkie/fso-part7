271a3265e5689b23eac2e703da3a904c7e8f7a49
philippOkie <kriukphilipp@gmail.com>
2023-12-24 19:41:07 +0200

    add Exercises 7.4.-7.6.

---
+++ routed-anecdotes-main/src/hooks/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onReset = () => {
+    setValue("");
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+    onReset,
+  };
+};
+
+// modules can have several named exports
+
+export const useCreateNew = () => {
+  // ...
+};
